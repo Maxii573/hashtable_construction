a2e03e1920ea21d61c00737c8d25f0f5cc7f9531
Maxi Rodriguez <maxirodriguez573@gmail.com>
2025-05-27 11:06:53 -0300

    REFACTORIZACIÓN -> Fase Verde: Conseguir pares clave-valor implementado

--- hash_table.py
+++ hash_table.py
@@ -22,26 +22,41 @@ Nuestra tabla hash va a tener otras características no escenciales pero que son
 - Haga que la tabla hash sea comparable utilizando el operador de prueba de igualdad
 - Mostrar una representación textual de la tabla hash
 '''
-BLANK = object()
-
 class Hashtable():
-    '''
-    En los siguientes avances, vamos a refactorizar la tabla hash
-    '''
     def __init__(self, capacity):
-        self.values = capacity * [BLANK]
+        '''
+        En primer lugar, podríamos cambiar el nombre de la variable .value a .pairs
+        ya que vamos a trabajar sobre artículos en ves de los valores en si
+        '''
+        self.pairs = capacity * [None]
 
     def __len__(self):
-        return len(self.values)
+        return len(self.pairs)
     
     def __setitem__(self, key, value):
-        self.values[self._index(key)] = value
+        '''
+        Ya que vamos a implementar articulos, vamos a guardar los nuevos elementos
+        por una tupla donde tendrá el par clave-valor.
+        Tendremos que tener en cuenta que incialmente la lista tendrá 2 tipos de
+        datos, las tuplas donde estará el par clave-valor y un objeto que 
+        representará una ranura vacía.
+        Ya que los datos de entrada estarán aisladas en las tuplas, ya no tendremos
+        que utilizar un objeto especial para indicar una ranura vacía, asi que podremos
+        utilizar None sin problemas, por que no va a interferir con el dato del usuario.
+        '''
+        self.pairs[self._index(key)] = (key, value)
 
     def __getitem__(self, key):
-        value = self.values[self._index(key)]
-        if value is BLANK:
+        '''
+        Necesitaremos refactorizar el método especial __getitem__, donde solamente
+        va cambiar lo que va a retornar, ya que la variable pair, es un None o 
+        una tupla, si este es una tupla va a retornar el segundo elemento de la tupla
+        que será el valor. Sino, va a levantar un KeyError.
+        '''
+        pair = self.pairs[self._index(key)]
+        if pair is None:
             raise KeyError(key)
-        return value
+        return pair[1]
     
     def __contains__(self, key):
         try:
@@ -50,7 +65,7 @@ class Hashtable():
             return False
         else:
             return True
-        
+
     def get(self, key, default=None):
         try:
             return self[key]
@@ -58,8 +73,15 @@ class Hashtable():
             return default
     
     def __delitem__(self, key):
+        '''
+        Tendremos que cambiar la implementación del método especial __delitem__,
+        ya que la nueva implementación del método especial __setitem__, asignará
+        una tupla, y __delitem__ va a crear una tupla con None, y es lo que no queremos
+        Ahora, vamos a cambiar la forma de buscar el elemento, que este elemento será
+        una tupla y esta tupla se le sobreescribirá por un None.
+        '''
         if key in self:
-            self[key] = BLANK
+            self.pairs[self._index(key)] = None
         else:
             raise KeyError(key)
 
--- test_hashtable.py
+++ test_hashtable.py
@@ -1,15 +1,13 @@
 from hash_table import Hashtable
 import pytest
+
 @pytest.fixture
 def tabla_hash():
     '''
-    Ahora, lo que vamos a hacer es refactorizar. En nuestra tabla hash, solo vamos
-    a poder manipular valores en una lista, no se guardan las claves, en los 
-    diccionarios en python, podremos iterar sobre sus claves, valores o pares de 
-    clave y valor llamados artículos (pairs), pero nuestra tabla hash no tiene
-    esta capacidad de guardar las claves para iterar o manipular sobre artículos.
+    Ahora estaremos en la fase verde ya que los elementos se almacenarán en pares
+    de clave-valor
     '''
-    tabla = Hashtable(capacity=30)
+    tabla = Hashtable(capacity=100)
     tabla["Nombre"] = "Maxi"
     tabla["Edad"] = 19
     tabla["Pais"] = "Argentina"
@@ -17,9 +15,31 @@ def tabla_hash():
     return tabla
 
 def test_tabla(tabla_hash):
-    assert ("Nombre", "Maxi") in tabla_hash
-    assert ("Edad", 19) in tabla_hash
-    assert ("Pais", "Argentina") in tabla_hash
-    assert ("Acceso", "True") in tabla_hash
+    assert ("Nombre", "Maxi") in tabla_hash.pairs
+    assert ("Edad", 19) in tabla_hash.pairs
+    assert ("Pais", "Argentina") in tabla_hash.pairs
+    assert ("Acceso", True) in tabla_hash.pairs
 
-   
+    assert len(tabla_hash) == 100
+
+def test_no_deberia_contener_el_valor_none_cuando_se_crea():
+    '''
+    En esta prueba, vamos a verificar que cuando se crea tabla hash, este no tenga
+    None predeterminado como valor en una tupla. Esto nos ayudará a prevenir el 
+    problema de que el valor None de las ranuras vacías no se mezclen con el None 
+    del usuario.
+    '''
+    hash_table = Hashtable(capacity=100)
+    values = [pair.value for pair in hash_table.pairs if pair]
+    assert None not in values
+
+def test_deberia_eliminar_par_clave_valor(tabla_hash):
+    assert "Nombre" in tabla_hash
+    assert ("Nombre", "Maxi") in tabla_hash.pairs
+
+    del tabla_hash["Nombre"]
+    
+    assert "Nombre" not in tabla_hash
+    assert ("Nombre", "Maxi") not in tabla_hash.pairs
+    assert len(tabla_hash) == 100
+    

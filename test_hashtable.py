5661025b5edc27a546d3a5aee60ecff6f3a934e0
Maxi Rodriguez <maxirodriguez573@gmail.com>
2025-07-08 17:26:46 -0300

    Fase roja: Copia defensiva no implementada

--- test_hashtable.py
+++ test_hashtable.py
@@ -3,10 +3,6 @@ import pytest
 
 @pytest.fixture
 def tabla_hash():
-    '''
-    Ahora estaremos en la fase verde ya que los elementos se almacenarán en pares
-    de clave-valor
-    '''
     tabla = Hashtable(capacity=100)
     tabla["Nombre"] = "Maxi"
     tabla["Edad"] = 19
@@ -14,32 +10,16 @@ def tabla_hash():
     tabla["Acceso"] = True
     return tabla
 
-def test_tabla(tabla_hash):
-    assert ("Nombre", "Maxi") in tabla_hash.pairs
-    assert ("Edad", 19) in tabla_hash.pairs
-    assert ("Pais", "Argentina") in tabla_hash.pairs
-    assert ("Acceso", True) in tabla_hash.pairs
-
-    assert len(tabla_hash) == 100
-
-def test_no_deberia_contener_el_valor_none_cuando_se_crea():
+def test_deberia_retornar_una_copia_de_los_pares(tabla_hash):
     '''
-    En esta prueba, vamos a verificar que cuando se crea tabla hash, este no tenga
-    None predeterminado como valor en una tupla. Esto nos ayudará a prevenir el 
-    problema de que el valor None de las ranuras vacías no se mezclen con el None 
-    del usuario.
+    En esta sección vamos a proteger nuestros datos. En la práctica, los métodos 
+    de acceso nunca deberian filtrar su implementación interna al usuario
+    ya que este puede alterar intencionalmente o voluntariamente. 
+    En este caso,.pairs donde tiene todos los elementos,
+    se expone como un atributo público.
+    Para que esto no suceda deberíamos darle al usuario una copia defensiva para
+    proteger atributos mutables de modificaciones externas.
+    Claramanete estamos en la fase roja por que no se esta haciendo una copia 
+    de pairs, se le está dando la implementación interna directamente.
     '''
-    hash_table = Hashtable(capacity=100)
-    values = [pair.value for pair in hash_table.pairs if pair]
-    assert None not in values
-
-def test_deberia_eliminar_par_clave_valor(tabla_hash):
-    assert "Nombre" in tabla_hash
-    assert ("Nombre", "Maxi") in tabla_hash.pairs
-
-    del tabla_hash["Nombre"]
-    
-    assert "Nombre" not in tabla_hash
-    assert ("Nombre", "Maxi") not in tabla_hash.pairs
-    assert len(tabla_hash) == 100
-    
+    assert tabla_hash.pairs is not tabla_hash.pairs
